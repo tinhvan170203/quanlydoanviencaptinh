9b64d00675e53ce34d151f8c4c88d555c90387a7
Tinh <vuvantinh121123@gmial.com>
2024-01-10 09:56:16 +0700

    success

--- routes/notification.js
+++ routes/notification.js
@@ -1,9 +1,9 @@
-const express = require('express');
+import express from 'express';
 
 const router = express.Router();
-const checkRole = require('../middlewares/checkRole');
-const middlewareController = require('../middlewares/verifyToken');
-const notification = require('../controllers/notification');
+import checkRole from '../middlewares/checkRole';
+import middlewareController from '../middlewares/verifyToken';
+import notification from '../controllers/notification';
 
 router.get('/fetch',middlewareController.verifyToken, notification.getNotifications);
 router.post('/add',middlewareController.verifyToken, checkRole("thêm thông báo"), notification.addNotification);
