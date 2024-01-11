c5bb51203ce48064ac0e5153a8251a3bfe852e24
Tinh <vuvantinh121123@gmial.com>
2024-01-11 08:11:41 +0700

    deploy backend

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ app.use(cookies());
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 //   });
-  
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
 app.use(cors({
     origin: ["http://localhost:5173", "http://localhost:3000", "https://quanlydoanvienhungyen.vercel.app", "https://quanlydoanvienhungyen.vercel.app"],
     credentials: true,
