233c9fcfa3f7b16e810f4a68d3d35b19b27a476d
Tinh <vuvantinh121123@gmial.com>
2024-01-12 11:05:51 +0700

    success

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(cookies());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 app.use(cors({
