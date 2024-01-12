96c0a7cc90644e09b662a6ffc317c51b493430a8
Tinh <vuvantinh121123@gmial.com>
2024-01-12 10:34:01 +0700

    success

--- index.js
+++ index.js
@@ -9,17 +9,11 @@ var bodyParser = require('body-parser')
 app.use(cookies());
 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
-//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
-//   })
+//   });
 app.use(cors({
     origin: ["https://quanlydoanvienhungyen.vercel.app"],
     credentials: true,
