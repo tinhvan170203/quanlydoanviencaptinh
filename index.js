acef4c319672023be63d94060018feed4508cf8f
Tinh <vuvantinh121123@gmial.com>
2024-01-12 09:40:28 +0700

    123123

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
   })
 // app.use(express.json());
