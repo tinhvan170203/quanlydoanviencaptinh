4f02276c7a262ff06e6b5e551fc9d6c40881e100
Tinh <vuvantinh121123@gmial.com>
2024-01-12 09:06:32 +0700

    123123

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cookies());
 //   });
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
   })
