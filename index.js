a92a56725555b7be4f7431d275435312bf3d4ca7
Tinh <vuvantinh121123@gmial.com>
2024-01-15 10:54:05 +0700

    xong end 343

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(cookies());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Origin', 'https://quanlydoanvienhungyen.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
   });
   
