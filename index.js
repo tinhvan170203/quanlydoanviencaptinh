26a273e887997618cc1acf0580021f521f6539ef
Tinh <vuvantinh121123@gmial.com>
2024-01-15 10:50:56 +0700

    xong end 343

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ var bodyParser = require('body-parser')
 app.use(cookies());
 
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.use(cors({
     // origin: "*",
     // credentials: true,
