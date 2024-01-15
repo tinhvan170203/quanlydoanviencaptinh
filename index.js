37d71a73b866b4e4c07a59ffc276bb3b6e257c9b
Tinh <vuvantinh121123@gmial.com>
2024-01-15 10:48:36 +0700

    xong end

--- index.js
+++ index.js
@@ -9,16 +9,17 @@ var bodyParser = require('body-parser')
 app.use(cookies());
 
 
-app.use(cors({
-    // origin: "*",
-    credentials: true,
-}));
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+  
+app.use(cors({
+    // origin: "*",
+    // credentials: true,
+}));
+
 
 
 // app.use(express.json());
