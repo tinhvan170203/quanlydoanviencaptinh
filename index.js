322776b145aa7dddbe8e682b6e9e2d34d8d96db4
Tinh <vuvantinh121123@gmial.com>
2024-01-15 08:52:17 +0700

    xong end

--- index.js
+++ index.js
@@ -9,17 +9,16 @@ var bodyParser = require('body-parser')
 app.use(cookies());
 
 
-
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-  });
 app.use(cors({
     origin: ["https://quanlydoanvienhungyen.vercel.app/","https://quanlydoanvienhungyen.vercel.app"],
     credentials: true,
 }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 
 // app.use(express.json());
